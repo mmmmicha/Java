3b0c507d478ee2b0795beaba156fe18d0ec2b4d1
wjdrhkd456 <56371387+wjdrhkd456@users.noreply.github.com>
2020-06-17 23:07:47 +0900

    List

---
+++ src/javaStudy/Test164.java
@@ -0,0 +1,117 @@
+package src.javaStudy;
+/*=======================================================
+  ■■■ 컬렉션 프레임워크(Collection Framework) ■■■
+=======================================================*/
+
+// List →Vector, ArrayList
+// ·순서 있음
+// ·배열과 유사
+// ·중복 허용
+
+
+
+/*
+○ ArrayList 클래스
+
+   - ArrayList 와 Vector 클래스는
+     List 를 크기 변경이 가능한 배열로 구현한 것으로
+	 차이점은 Vector 클래스의 객체는 기본적으로 동기화 되지만,
+	 ArrayList 는 그렇지 않다는 점이다.
+
+   - 비동기화 컬렉션은 동기화된 컬렉션보다 더 나은 성능을 제공하며
+     스레드들이 컬렉션을 공유하지 않는 프로그램에서는
+	 일반적으로 ArrayList 를 Vector 보다 더 선호하며,
+	 ArrayList 는 동기화를 고려하지 않은 Vector 처럼 동작함으로써
+	 스레드 동기화에 따르는 부담을 가지지 않기 때문에
+	 Vector 보다 더 빠르게 실행된다.
+
+   - null 을 포함한 모든 요소를 허용하며
+     List 인터페이스를 구현하는 것 외에 리스트에 격납하기 위해
+	 내부적으로 사용되는 배열의 사이즈를 조작하는 메소드를 제공한다.
+*/
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Test164
+{
+	public static void main(String[] args)
+	{
+		// ArrayList 자료구조 인스턴스 생성
+		//ArrayList<String> list = new ArrayList<String>();
+		//Vector<String> list = new Vector<String>();
+		//List<String> list = new Vector<String>();
+		List<String> list1 = new ArrayList<String>();
+
+		// list1 에 요소 추가 → add()
+		list1.add("봉오동전투");
+		list1.add("알라딘");
+		list1.add("토이스토리");
+		
+		// ArrayList 자료구조 인스턴스 생성
+		List<String> list2 = new ArrayList<String>(list1);
+		//--==>> 얕은복사가 아니라 인스턴스를 새로 만들면서 그 내용들을 일일이 넣어주는 모습
+
+		// list2 에 요소 추가
+		list2.add("엑시트");
+
+		// ArrayList 자료구조 인스턴스 생성
+		List<String> list3 = new ArrayList<String>();
+
+		// list3 에 list2 의 전체 요소 추가 → addAll()
+		list3.addAll(list2);
+		
+		// "알라딘" 앞에 "겨울왕국" 추가
+		//-- ① "알라딘" 찾기 → indexOf() → 위치값 인덱스 반환
+		int n = list3.indexOf("알라딘");
+
+		// 테스트
+		System.out.println(n);	//--==>> 1
+
+		//-- ② "알라딘" 찾은 인덱스 위치에 "겨울왕국" 투입(삽입)
+		list3.add(n, "겨울왕국");
+
+
+
+		// 출력 → 더미 데이터 확인
+		System.out.println("==========================================");
+		System.out.println(list1);
+		//--==>> [봉오동전투, 알라딘, 토이스토리]
+		System.out.println(list2);
+		//--==>> [봉오동전투, 알라딘, 토이스토리, 엑시트]
+		System.out.println(list3);
+		//--==>> [봉오동전투, 겨울왕국, 알라딘, 토이스토리, 엑시트]
+
+		System.out.println();
+
+		// 출력 → listIterator() 메소드를 활용하여 출력
+		System.out.println("==========================================");
+		ListIterator<String> li = list3.listIterator();
+		while (li.hasNext())
+		{
+			System.out.print(li.next() + " ");
+		}
+		System.out.println("\n");
+		//												     ▼
+		//--==>> 봉오동전투 겨울왕국 알라딘 토이스토리 엑시트
+		//							↓
+		//							↓ 이어 받아
+
+
+		// 출력 → 역순으로 출력
+		System.out.println("==========================================");
+		while (li.hasPrevious())
+		{
+			System.out.print(li.previous() + " ");
+		}
+		System.out.println("\n");
+		//--==>> 엑시트 토이스토리 알라딘 겨울왕국 봉오동전투
+
+
+
+
+
+
+	}
+
+}
---
+++ src/javaStudy/Test165.java
@@ -0,0 +1,171 @@
+package src.javaStudy;
+/*=======================================================
+  ■■■ 컬렉션 프레임워크(Collection Framework) ■■■
+=======================================================*/
+
+/*
+○ List 인터페이스
+   - 배열과 유사, 순서가 있음, 중복을 허용
+   - 주요 구현 클래스
+     ·Vector(동기화 지원)
+	 ·ArrayList(동기화 지원하지 않음)
+	 ·LinkedList(체인, 링크, 결합)
+*/
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class Test165
+{
+	public static void main(String[] args)
+	{
+		List<String> list = new ArrayList<String>();
+		// ArrayList : List 인터페이스를 구현한 클래스이기 때문에 가능한 구문
+
+		list.add("서울");
+		list.add("부산");
+		list.add("인천");
+		list.add("대구");
+		list.add("광주");
+		list.add("대전");
+		list.add("울산");
+		list.add("세종");
+
+		System.out.println(list);
+		//--==>> [서울, 부산, 인천, 대구, 광주, 대전, 울산, 세종]
+
+		System.out.println("전체 갯수 : " + list.size());
+		//--==>> 전체 갯수 : 8
+
+		// 첫 번째 요소만 출력
+		System.out.println("첫 번째 요소 : " + list.get(0));
+		//--==>> 첫 번째 요소 : 서울
+
+		// 마지막 요소만 출력
+		System.out.println("마지막 요소 : " + list.get(list.size()-1));
+		//--==>> 마지막 요소 : 세종
+
+		// 처음 위치(index : 0)에 "한국" 추가
+		list.add(0, "한국");
+		System.out.println(list);
+		//--==>> [한국, 서울, 부산, 인천, 대구, 광주, 대전, 울산, 세종]
+
+		// 처음 위치 데이터를 "대한민국"으로 변경
+		list.set(0, "대한민국");
+		System.out.println(list);
+		//--==>> [대한민국, 서울, 부산, 인천, 대구, 광주, 대전, 울산, 세종]
+
+		// "서울" 존재여부 확인
+		if (list.contains("서울"))
+			System.out.println("서울이 존재합니다.");
+		//--==>> 서울이 존재합니다.
+
+		// "서울"이 몇 번째 인덱스에 존재하는지 확인
+
+		int n = list.indexOf("서울");
+		System.out.println("서울의 위치 : " + n);
+		//--==>> 서울의 위치 : 1
+
+		// "대한민국" 지우기
+		list.remove(0);
+		//list.remove("대한민국");
+		System.out.println(list);
+		//--==>> [서울, 부산, 인천, 대구, 광주, 대전, 울산, 세종]
+		//--==>> [서울, 부산, 인천, 대구, 광주, 대전, 울산, 세종]
+
+		// ArrayList 자료구조 list 의 전체 요소 출력
+		System.out.println("전체 출력 ... ①");
+		for(int idx=0; idx<list.size(); idx++)
+			System.out.print(list.get(idx) + " ");
+		System.out.println();
+		//--==>> 서울 부산 인천 대구 광주 대전 울산 세종
+
+		System.out.println("역순 출력 ... ①");
+		for(int idx=list.size()-1; idx>=0; idx--)
+			System.out.print(list.get(idx) + " ");
+		System.out.println();
+		//--==>>
+		/*
+		역순 출력 ...
+		세종 울산 대전 광주 대구 인천 부산 서울
+		*/
+
+		System.out.println("전체 출력 ... ②");
+		for(String str : list)
+			System.out.print(str + " ");
+		System.out.println();
+		//--==>>
+		/*
+		전체 출력 ... ②
+		서울 부산 인천 대구 광주 대전 울산 세종
+		*/
+
+		System.out.println("전체 출력 ... ③");
+		Iterator<String> it = list.iterator();
+		while(it.hasNext())
+		{
+			String s = it.next();
+			System.out.print(s + " ");
+		}
+		System.out.println();
+		//--==>>
+		/*
+		전체 출력 ... ③
+		서울 부산 인천 대구 광주 대전 울산 세종
+		*/
+
+		System.out.println("전체 출력 ... ④");
+		ListIterator<String> li = list.listIterator();
+		while (li.hasNext())
+		{
+			System.out.print(li.next() + " ");
+		}
+		System.out.println();
+		//--==>>
+		/*
+		전체 출력 ... ④
+		서울 부산 인천 대구 광주 대전 울산 세종
+		*/
+
+		System.out.println("역순 출력 ... ②");
+		while (li.hasPrevious())
+		{
+			String s = li.previous();
+			System.out.print(s + " ");
+		}
+		System.out.println();
+		//--==>>
+		/*
+		역순 출력 ... ②
+		세종 울산 대전 광주 대구 인천 부산 서울
+		*/
+
+
+		System.out.println("역순으로 출력 ... ③");
+		ListIterator<String> li2 = list.listIterator(list.size());
+		while (li2.hasPrevious()/*li2.hasNext()*/)
+		{
+			String s = li2.previous();
+			//String s = li2.next();
+			System.out.print(s + " ");
+		}
+		System.out.println();
+		//--==>>
+		/*
+		역순으로 출력 ... ③
+		세종 울산 대전 광주 대구 인천 부산 서울
+		*/
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
